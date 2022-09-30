80d17fd71cbf21d272d79aac99f798a57de6292e
mikk369 <mikk.ereline@vikk.ee>
2022-09-30 09:35:39 +0300

    added all

---
+++ client/src/router/index.js
@@ -0,0 +1,54 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
+import addMovie from '../views/addMovie.vue';
+import Post from '../views/Post.vue';
+import signup from '../views/signup.vue';
+import login from '../views/Login.vue';
+import Header from '../views/Header.vue';
+import EditPost from '../views/EditPost.vue';
+Vue.use(VueRouter);
+
+Vue.component('Header', Header);
+
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+  },
+  {
+    path: '/addMovie',
+    name: 'addMovie',
+    component: addMovie,
+  },
+  {
+    path: '/post/:id',
+    name: 'post',
+    component: Post,
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: login,
+  },
+  {
+    path: '/edit-post/:id',
+    name: 'edit-post',
+    component: EditPost,
+  },
+  {
+    path: '/signup',
+    name: 'signup',
+    component: signup,
+  },
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.zBASE_URL,
+  routes,
+});
+
+export default router;
---
+++ client/src/views/Login.vue
@@ -0,0 +1,100 @@
+<template>
+    <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lato:wght@300&family=Lexend+Deca:wght@300;400&family=Montserrat:ital,wght@1,900&family=Poppins:wght@300;400;600;700;800&family=Roboto:wght@500;900&display=swap"
+          rel="stylesheet"
+        />
+  
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css"
+          integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA=="
+          crossorigin="anonymous"
+          referrerpolicy="no-referrer"
+        />
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  
+        <title>WebShop</title>
+      </head>
+  
+        <Header></Header>
+
+  <form id="form">
+      <h3 class="loginTag">Login</h3>
+        <div class="emailInput">
+          <input id="email" type="email"  placeholder="Email">
+        </div>
+
+        <div class="passwordInput">
+          <input id="password" type="password"  placeholder="password">
+        </div>
+        
+        <button class=" inputSubmitButton btn btn-primary btn-block">login</button>
+      </form>
+      
+      </html>
+    </template>
+    
+    <style>
+      
+      input {
+        padding: 5px;
+      }
+      .loginTag{
+        top: 25%;
+        position: absolute;
+        right: 47%;
+      }
+      .emailInput {
+        top: 35%;
+        position: absolute;
+        right: 50%;
+        padding: 5px;
+        border-bottom: 1px solid white;
+        
+      }
+      .passwordInput {
+        top: 35%;
+        position: absolute;
+        right: 32%;
+        padding: 5px;
+        border-bottom: 1px solid white;
+      }
+  .inputSubmitButton {
+    position: absolute;
+    right: 24%;
+    top: 34%;
+  }
+  </style>
+  
+  <script>
+    
+    import axios from 'axios';
+    // import login from '../../login.js';
+    export default {
+      name: "Login",
+      data(){
+          return {
+             email: "",
+             password: ""
+            }
+          },
+    methods:{
+        async handleSubmit(){
+              const response = await axios.post("login", {
+                  email: this.email,
+                  password: this.password
+                });
+                console.log(response)
+            }
+          }
+       }
+      
+          
+      
+        </script>
