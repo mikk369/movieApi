1ba62d1873c922b3d67019790a25a90e6be7943d
mikk369 <mikk.ereline@vikk.ee>
2022-09-30 13:35:35 +0300

    removed req.body.name

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -3,7 +3,6 @@ const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
-
 const signToken = id => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN
@@ -36,7 +35,6 @@ const createSendToken = (user, statusCode, res) => {
 
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
-    name: req.body.name,
     email: req.body.email,
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm
