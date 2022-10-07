3ea62bc71ce93ce5972dbb73c98902ec80527b0e
mikk369 <mikk.ereline@vikk.ee>
2022-10-07 17:21:16 +0300

    temporary removed authcontrollers

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -21,12 +21,12 @@ let upload = multer({
 router
   .route('/')
   .get(postApi.getAllPosts)
-  .post(authController.protect, upload, postApi.createPost);
+  .post(upload, postApi.createPost);
 
 router
   .route('/:id')
-  .get(authController.protect, postApi.getPostById)
-  .patch(authController.protect, upload, postApi.updatePost)
-  .delete(authController.protect, postApi.deletePost);
+  .get(postApi.getPostById)
+  .patch(upload, postApi.updatePost)
+  .delete(postApi.deletePost);
 
 module.exports = router;
