baf4e28ec1ed56a9df5282be98d806e286134ab1
mikk369 <mikk.ereline@vikk.ee>
2023-09-18 20:35:08 +0300

    fixed login and signup

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,154 +1,92 @@
-const { promisify } = require('util');
-const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
-const catchAsync = require('./../utils/catchAsync');
-const AppError = require('./../utils/appError');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-const signToken = id => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN
+//token function so dont have to repeat code
+const signToken = (id, username) => {
+  return jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: 1 * 24 * 60 * 60 * 1000,
   });
 };
 
-const createSendToken = (user, statusCode, req, res) => {
-  const token = signToken(user._id);
+exports.signup = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
 
-  res.cookie('jwt', token, {
-    expires: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-    ),
-    httpOnly: true,
-    secure: req.secure || req.headers['x-forwarded-proto'] === 'https'
-  });
+    //regex for checking if email is valid
+    const checkEmail = /\S+@\S+\.\S+/;
+    // check if email is valid and password length atleast 8
+    if (checkEmail.test(email) && password.length > 8) {
+      let user = await User.findOne({ email });
+      if (!user) {
+        const createUser = new User({
+          email: email,
+          password: password,
+        });
+        //token
+        const token = signToken(createUser._id);
 
-  // Remove password from output
-  user.password = undefined;
-
-  res.status(statusCode).json({
-    status: 'success',
-    token,
-    data: {
-      user
+        await User.create(createUser);
+        res.status(201).json({
+          message: 'User Created',
+          token,
+          data: {
+            user: createUser,
+          },
+        });
+      } else {
+        res.status(403).json({ error: 'User already exists' });
+      }
+    } else {
+      res.status(406).send({ error: 'Email or password not acceptable' });
     }
-  });
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
 };
 
-exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create({
-    email: req.body.email,
-    password: req.body.password,
-    passwordConfirm: req.body.passwordConfirm
-  });
+exports.login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
 
-  const url = `${req.protocol}://${req.get('host')}/me`;
-  // console.log(url);
-
-  createSendToken(newUser, 201, req, res);
-});
-
-exports.login = catchAsync(async (req, res, next) => {
-  const { email, password } = req.body;
-
-  // 1) Check if email and password exist
-  if (!email || !password) {
-    return next(new AppError('Please provide email and password!', 400));
-  }
-  // 2) Check if user exists && password is correct
-  const user = await User.findOne({ email }).select('+password');
-
-  if (!user || !(await user.correctPassword(password, user.password))) {
-    return next(new AppError('Incorrect email or password', 401));
-  }
-
-  // 3) If everything ok, send token to client
-  createSendToken(user, 200, req, res);
-});
-
-exports.logout = (req, res) => {
-  res.cookie('jwt', 'loggedout', {
-    expires: new Date(Date.now() + 10 * 1000),
-    httpOnly: true
-  });
-  res.status(200).json({ status: 'success' });
-};
-
-exports.protect = catchAsync(async (req, res, next) => {
-  // 1) Getting token and check of it's there
-  let token;
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith('Bearer')
-  ) {
-    token = req.headers.authorization.split(' ')[1];
-  } else if (req.cookies.jwt) {
-    token = req.cookies.jwt;
-  }
-
-  if (!token) {
-    return next(
-      new AppError('You are not logged in! Please log in to get access.', 401)
-    );
-  }
-
-  // 2) Verification token
-  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
-
-  // 3) Check if user still exists
-  const currentUser = await User.findById(decoded.id);
-  if (!currentUser) {
-    return next(
-      new AppError(
-        'The user belonging to this token does no longer exist.',
-        401
-      )
-    );
-  }
-
-  // 4) Check if user changed password after the token was issued
-  if (currentUser.changedPasswordAfter(decoded.iat)) {
-    return next(
-      new AppError('User recently changed password! Please log in again.', 401)
-    );
-  }
-
-  // GRANT ACCESS TO PROTECTED ROUTE
-  req.user = currentUser;
-  res.locals.user = currentUser;
-  next();
-});
-
-// Only for rendered pages, no errors!
-exports.isLoggedIn = async (req, res, next) => {
-  if (req.cookies.jwt) {
-    try {
-      // 1) verify token
-      const decoded = await promisify(jwt.verify)(
-        req.cookies.jwt,
-        process.env.JWT_SECRET
+    //check if email and password exist
+    if (!email || !password) {
+      //call next so login function finish right away
+      return next(res.status(400).json({ error: 'Please provide email and password!' }));
+    }
+    //check if user exist && password is correct
+    const user = await User.findOne({ email }).select('+password');
+    //if user does not exist it stops there , if user exist it runs entire code
+    if (!user || !(await user.correctPassword(password, user.password))) {
+      return next(
+        res.status(401).json({
+          error: 'Incorrect email or password',
+        })
       );
-
-      // 2) Check if user still exists
-      const currentUser = await User.findById(decoded.id);
-      if (!currentUser) {
-        return next();
-      }
-
-      // 3) Check if user changed password after the token was issued
-      if (currentUser.changedPasswordAfter(decoded.iat)) {
-        return next();
-      }
-
-      // THERE IS A LOGGED IN USER
-      res.locals.user = currentUser;
-      return next();
-    } catch (err) {
-      return next();
     }
+    //if everything ok ,send token to client
+    const token = signToken(user._id);
+    //  put token into cookie
+    res.cookie('jwt', token, {
+      expires: new Date(
+        // convert time in milliseconds
+        Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+      ),
+      //when using HTTPS then cookie will be created then use secure: true
+      // secure: true,
+      httpOnly: true,
+    });
+    //remove password from output
+    user.password = undefined;
+
+    res.status(200).json({
+      status: 'success',
+      token,
+      data: {
+        user: user,
+      },
+    });
+  } catch (error) {
+    res.status(500).send({ error: error.message });
   }
-  next();
 };
-
-
-
-
-
