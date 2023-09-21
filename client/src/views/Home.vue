8691c30c8c81c0d4eca87046c97a29622982c314
mikk369 <mikk.ereline@vikk.ee>
2023-09-21 17:44:53 +0300

    centered card row

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -5,16 +5,14 @@
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
       <link
         href="https://fonts.googleapis.com/css2?family=Lato:wght@300&family=Lexend+Deca:wght@300;400&family=Montserrat:ital,wght@1,900&family=Poppins:wght@300;400;600;700;800&family=Roboto:wght@500;900&display=swap"
-        rel="stylesheet"
-      />
+        rel="stylesheet" />
 
       <link
         rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css"
         integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA=="
         crossorigin="anonymous"
-        referrerpolicy="no-referrer"
-      />
+        referrerpolicy="no-referrer" />
       <meta charset="UTF-8" />
       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -28,14 +26,19 @@
       color="green accent-4"
       dark
       dismissible
-      v-if="this.$route.params.message"
-    >
+      v-if="this.$route.params.message">
       {{ this.$route.params.message }}
     </v-alert>
 
-    <Card></Card>
+    <Card :posts="posts"></Card>
   </html>
 </template>
+
+<style scoped>
+.row {
+  justify-content: center;
+}
+</style>
 
 <script>
 import Header from './Header.vue';
@@ -47,15 +50,14 @@ export default {
   data() {
     return {
       posts: [],
-      connection: null
+      connection: null,
     };
   },
 
-  
   async created() {
     this.posts = await API.getAllPosts();
   },
- 
+
   components: { Header, Card },
 };
 </script>
