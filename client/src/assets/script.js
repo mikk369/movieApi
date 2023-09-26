e66bb0cdaa43662824501cef4f22f772cc905f8f
mikk369 <mikk.ereline@vikk.ee>
2023-09-26 18:32:51 +0300

    websocket added

--- client/src/assets/script.js
+++ client/src/assets/script.js
@@ -1,7 +1,11 @@
 'use strict';
-
 //this listens for DOM content loaded just in case
 document.addEventListener('DOMContentLoaded', () => {
+  window.addEventListener('pageshow', function (event) {
+    if (event.persisted && window.location.pathname === '/') {
+      window.location.reload();
+    }
+  });
   //apikey
   const apikey = '9f0ca0df';
   //search movies from API
@@ -140,119 +144,4 @@ document.addEventListener('DOMContentLoaded', () => {
       document.getElementsByClassName('submit-button').click();
     }
   });
-  // Check if movies exist in local storage and it's an array
-  if (window.location.pathname === '/') {
-    //movies array from localstorage
-    let movieArr = [];
-    //gets items from localstorage and parses back to JSON
-    let movies = JSON.parse(localStorage.getItem('movies'));
-    // check if moveArr has data
-    if (movies !== null) {
-      //loops over the items from localstorage and pushes to array
-      for (let i = 0; i < movies.length; i++) {
-        movieArr.push(movies[i]);
-      }
-    }
-    let reverseArr = movieArr.reverse();
-    // console.log(movieArr)
-    reverseArr.forEach((localMovies) => {
-      // rendering movies from localstorage
-      const movieContainer = document.createElement('div');
-      //give it a classname
-      movieContainer.classList.add('card');
-      //create innerHtml for div
-      movieContainer.innerHTML = `
-            <div class="card-box">
-                  <img
-                    class="poster"
-                    src="${localMovies.Poster}"
-                    alt="Avatar"
-                    height="300" width="200"
-                  />
-                  <div class="lower-card">
-                  <h4 class="movie-name"><b>${localMovies.Title}</b></h4>
-                    <p class="year">${localMovies.Year}</p>
-                    <div class="liked-button-wrapper">
-                    <button class="like-button-heart" data-movie="${localMovies.imdbID}"><svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="currentColor"
-                    class="like-button-heart">
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
-                      </svg>
-                    </button>
-                      </div>
-                  </div>
-                </div> `;
-      const likeButton = movieContainer.querySelector('.like-button-heart');
-      // Get the JWT token from localStorage
-      const jwtToken = localStorage.getItem('token');
-
-      // Find the like button element by its class or ID
-
-      if (jwtToken) {
-        // Token exists in localStorage, display the like button
-        likeButton.style.display = 'block'; // or 'inline-block', 'inline', etc., depending on your styling
-      } else {
-        // Token does not exist in localStorage, hide the like button
-        likeButton.style.display = 'none';
-      }
-      // Retrieve apiLikedMovies from local storage or initialize as an empty array
-      let apiLikedMovies = JSON.parse(localStorage.getItem('apiLikedMovies')) || [];
-
-      // Check if the movie is already in the apiLikedMovies
-      const isAlreadyLiked = apiLikedMovies.includes(localMovies.imdbID);
-
-      if (isAlreadyLiked) {
-        // Add the "liked" class to change the button color to red
-        likeButton.classList.add('liked');
-      }
-      likeButton.addEventListener('click', () => {
-        const movieDataString = likeButton.getAttribute('data-movie');
-        const movieData = movieDataString;
-
-        // Get existing apiLikedMovies from local storage or initialize as an empty array
-        let apiLikedMovies = JSON.parse(localStorage.getItem('apiLikedMovies')) || [];
-        // Check if the movie is already in the apiLikedMovies
-        const isAlreadyLiked = apiLikedMovies.includes(movieData);
-
-        if (!isAlreadyLiked) {
-          // Add the movie to the liked movies array
-          apiLikedMovies.push(movieData);
-
-          // Update the liked movies in local storage
-          localStorage.setItem('apiLikedMovies', JSON.stringify(apiLikedMovies));
-
-          // Handle the Like action here (e.g., update the UI)
-          console.log('Liked:', movieData);
-
-          // Add a class to change the button color to red
-          likeButton.classList.add('liked');
-        } else {
-          // Remove the movie from the array by IMDb ID
-          apiLikedMovies = apiLikedMovies.filter((likedMovie) => likedMovie !== movieData);
-
-          // Update the liked movies in local storage
-          localStorage.setItem('apiLikedMovies', JSON.stringify(apiLikedMovies));
-
-          // Handle the Unlike action here (e.g., update the UI)
-          console.log('Unliked:', localMovies.Title);
-
-          // Remove the 'liked' class to change the button color back to normal
-          likeButton.classList.remove('liked');
-        }
-      });
-
-      //add movieContainer to movie-div
-      const rowElement = document.getElementsByClassName('row');
-      if (rowElement.length > 0) {
-        rowElement[0].appendChild(movieContainer);
-      }
-    });
-  }
 });
--- server/app.js
+++ server/app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-
-const cookieParser = require("cookie-parser")
-
+const cookieParser = require('cookie-parser');
+const WebSocket = require('ws'); // Import the 'ws' library
 
 const postRoutes = require('./routes/postRoutes');
 const userRoutes = require('./routes/userRoutes');
 const app = express();
+const server = require('http').createServer(app); // Create an HTTP server
 
 require('dotenv').config();
 
@@ -22,20 +22,65 @@ app.use(express.static('uploads'));
 // Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-  // console.log(req.cookies);
   next();
 });
-//DB connection 
+
+// DB connection
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.PASSWORD);
 mongoose.connect(DB, {
   useNewUrlParser: true,
 }).then;
-console.log('DB connection successfull!');
+console.log('DB connection successful!');
 
-//Mounting routers on 2 routes
+// Mounting routers on 2 routes
 app.use('/api/post', postRoutes);
 app.use('/api/users', userRoutes);
 
-app.listen(port, () => {
+// Create a WebSocket server
+const wss = new WebSocket.Server({ server });
+
+// WebSocket server logic
+wss.on('connection', (ws) => {
+  // Handle WebSocket connections here
+  ws.on('message', (message) => {
+    // Handle incoming WebSocket messages
+    console.log(`Received: ${message}`);
+
+    try {
+      const data = JSON.parse(message);
+
+      if (data.type === 'newMovie') {
+        // Handle the 'newMovie' message
+        // You can access the movie data using data.movie
+        console.log('Received a new movie:', data.movie);
+
+        // Broadcast the new movie to all connected clients
+        wss.clients.forEach((client) => {
+          if (client !== ws && client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify({ type: 'newMovie', movie: data.movie }));
+          }
+        });
+      } else if (data.type === 'deletePost') {
+        // Handle the 'deletePost' message
+        const postId = data.postId;
+
+        // Broadcast the delete event to all connected clients
+        wss.clients.forEach((client) => {
+          if (client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify({ type: 'deletePost', postId }));
+          }
+        });
+      }
+    } catch (error) {
+      console.error('Error parsing WebSocket message:', error);
+    }
+  });
+
+  // Send a welcome message to the newly connected client
+  ws.send('Welcome to the WebSocket server!');
+});
+
+// Start the HTTP server
+server.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
