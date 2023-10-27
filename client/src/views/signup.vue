2263201f74ba989d4dfdc5ea9ea6debe068e2772
Henno Täht <henno.taht@gmail.com>
2023-10-27 23:12:27 +0300

    Refactor Signup Vue Component for Enhanced Usability and Error Handling
    - Enhanced the structure of the HTML to improve readability and maintainability.
    - Included the use of type attributes for the input fields to improve user input validation.
    - Simplified the styling by removing unnecessary positioning and enhancing the appearance of the signup form.
    - Implemented dynamic error messaging to improve user feedback:
      - An error message will now display based on the response from the server.
      - Error messages will clear when the user modifies the input fields.
    - Streamlined the signup submission process to include error handling, providing a more robust user experience.

--- client/src/views/signup.vue
+++ client/src/views/signup.vue
@@ -1,111 +1,102 @@
 <template>
   <html lang="en">
-    <head>
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-      <link
+  <head>
+    <link rel="preconnect" href="https://fonts.googleapis.com"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+    <link
         href="https://fonts.googleapis.com/css2?family=Lato:wght@300&family=Lexend+Deca:wght@300;400&family=Montserrat:ital,wght@1,900&family=Poppins:wght@300;400;600;700;800&family=Roboto:wght@500;900&display=swap"
         rel="stylesheet"
-      />
+    />
 
-      <link
+    <link
         rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css"
         integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA=="
         crossorigin="anonymous"
         referrerpolicy="no-referrer"
-      />
-      <meta charset="UTF-8" />
-      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-      <meta
+    />
+    <meta charset="UTF-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta
         password="viewport"
         content="width=device-width, initial-scale=1.0"
-      />
-    </head>
+    />
+  </head>
+  <Header></Header>
 
-    <Header></Header>
+  <form class="sign-up-form" @submit.prevent="submitSignUp">
+    <h1>Sign Up</h1>
+    <div class="input-group">
+      <input type="email" placeholder="Email" v-model="email" required @input="clearError"/>
+    </div>
 
-    <form class="sign-up-form">
-      <h1>Sign Up</h1>
-      <div class="email">
-        <input type="text" placeholder="Email" v-model="email" />
-      </div>
+    <div class="input-group">
+      <input type="password" placeholder="Password" v-model="password" required @input="clearError"/>
+    </div>
 
-      <div class="pw1">
-        <input type="password" placeholder="Password" v-model="password" />
-      </div>
+    <div class="input-group">
+      <input type="password" placeholder="Confirm Password" v-model="passwordConfirm" required @input="clearError"/>
+    </div>
 
-      <div class="pw2">
-        <input
-          type="password"
-          placeholder="Confirm Password"
-          v-model="passwordConfirm"
-        />
-      </div>
+    <div v-if="errorMessage" class="error-message">
+      {{ errorMessage }}
+    </div>
 
-      <v-btn class="mr-4" v-on:click.prevent="submitSignUp"> submit </v-btn>
-    </form>
+    <v-btn class="submit-btn" v-on:click.prevent="submitSignUp">Submit</v-btn>
+  </form>
   </html>
 </template>
 
 <style>
-h1 {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin-bottom: 9px;
-}
-
 .sign-up-form {
-  position: relative;
-  height: 300px;
-  margin-top: 150px;
-}
-.email {
-  position: absolute;
-  left: 42%;
-}
-input{
-  color: #fff;
-
-}
-.pw1 {
-  position: absolute;
-  left: 42%;
-  top: 30%;
-}
-.pw2 {
-  position: absolute;
-  left: 42%;
-  top: 47%;
+  max-width: 400px;
+  margin: 150px auto;
+  padding: 20px;
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
 }
 
-.mr-4 {
-  position: absolute;
-  left: 47%;
-  top: 75%;
+.input-group {
+  margin-bottom: 20px;
+}
+
+.error-message {
+  color: red;
+  margin-bottom: 20px;
+}
+
+.submit-btn {
+  width: 100%;
 }
 </style>
 
 <script>
 import axios from 'axios';
+
 export default {
-  name: 'signup',
   data() {
     return {
       email: '',
       password: '',
       passwordConfirm: '',
+      errorMessage: '',
     };
   },
   methods: {
+    clearError() {
+      this.errorMessage = '';
+    },
     async submitSignUp() {
-      await axios.post('http://localhost:8000/api/users/signup', {
-        email: this.email,
-        password: this.password,
-        passwordConfirm: this.passwordConfirm,
-      });
-      this.$router.push('/login');
+      try {
+        await axios.post('http://localhost:8000/api/users/signup', {
+          email: this.email,
+          password: this.password,
+          passwordConfirm: this.passwordConfirm,
+        });
+        this.$router.push('/login');
+      } catch (err) {
+        this.errorMessage = err.response.data.error;
+      }
     },
   },
 };
